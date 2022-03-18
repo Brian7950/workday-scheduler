96a52c352d8b3500ec74e2747428045fc4871d88
Brian7950 <brianh7950@gmail.com>
2022-03-18 12:35:45 -0600

    place holder on reload

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,15 @@ var dateToday = moment();
 console.log(dateToday._d);
 
 var htmlCode = "";
+
 for(i = 9; i < 18; i++){
+    var storedEntry = localStorage.getItem(i) || "";
     htmlCode += ` <div class="row" >
     <h2>${i}</h2>
-    <textarea id="${i}" cols="90"></textarea>
+    <textarea id="${i}" value="${storedEntry}" placeholder="${storedEntry}" cols="90"></textarea>
     <button class="saveBtn">Save</button>
   </div>`
+
 }
 $(".container").html(htmlCode);
 
