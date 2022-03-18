8b61026e1f40b50f66f73621ece931808e5c0052
Brian7950 <brianh7950@gmail.com>
2022-03-18 12:41:50 -0600

    color added to timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,22 @@
 var dateToday = moment();
 console.log(dateToday._d);
-
+$("#currentDay").text(dateToday);
 var htmlCode = "";
+var currentHour = moment().hours()
 
 for(i = 9; i < 18; i++){
     var storedEntry = localStorage.getItem(i) || "";
+    if(i < currentHour){
+        var color = "past"
+    }
+    else if(i == currentHour){
+        var color = "present"
+    }else{
+        var color = "future"
+    }
     htmlCode += ` <div class="row" >
     <h2>${i}</h2>
-    <textarea id="${i}" value="${storedEntry}" placeholder="${storedEntry}" cols="90"></textarea>
+    <textarea class="${color}" id="${i}" value="${storedEntry}" placeholder="${storedEntry}" cols="90"></textarea>
     <button class="saveBtn">Save</button>
   </div>`
 
@@ -20,3 +29,4 @@ $(".saveBtn").click(function(){
     console.log(textInput,timeStamp);
     localStorage.setItem(timeStamp , textInput);
 })
+
